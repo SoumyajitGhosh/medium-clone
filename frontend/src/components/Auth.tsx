8b1a174b3637b42f166e7a346d42998bc5c22bd9
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-07-28 16:45:49 +0530

    Build:Frontend Auth Screens with published npm packages

---
+++ frontend/src/components/Auth.tsx
@@ -0,0 +1,111 @@
+import { ChangeEvent, useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  SignupInput,
+  SigninInput,
+} from "@_soumyajit.ghosh_/medium-clone-common";
+
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
+  const initialInputs =
+    type === "signup"
+      ? { name: "", email: "", password: "" }
+      : { email: "", password: "" };
+
+  const [postInputs, setPostInputs] = useState<SignupInput | SigninInput>(
+    initialInputs
+  );
+
+  return (
+    <div className="h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div>
+          <div className="px-10">
+            <div className="text-3xl font-extrabold">Create an account</div>
+            <div className="text-slate-400">
+              {type === "signup"
+                ? "Already have an account?"
+                : "Don't have an account?"}
+              <Link
+                className="pl-2 underline"
+                to={type === "signup" ? "/signin" : "/signup"}
+              >
+                {type === "signup" ? "Sign in" : "Sign up"}
+              </Link>
+            </div>
+          </div>
+          <div className="pt-8">
+            {type === "signup" ? (
+              <LabelledInput
+                label="Name"
+                placeholder="John Doe"
+                onChange={(e) => {
+                  setPostInputs((c) => ({
+                    ...c,
+                    name: e.target.value,
+                  }));
+                }}
+              />
+            ) : null}
+            <LabelledInput
+              label="Email"
+              placeholder="john.doe@gmail.com"
+              onChange={(e) => {
+                setPostInputs((c) => ({
+                  ...c,
+                  email: e.target.value,
+                }));
+              }}
+            />
+            <LabelledInput
+              label="Password"
+              type="password"
+              placeholder="123456"
+              onChange={(e) => {
+                setPostInputs((c) => ({
+                  ...c,
+                  email: e.target.value,
+                }));
+              }}
+            />
+            <button
+              type="button"
+              className="mt-8 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+            >
+              {type === "signup" ? "Sign up" : "Sign in"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+interface LabelledInputType {
+  label: string;
+  placeholder: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+
+function LabelledInput({
+  label,
+  placeholder,
+  type,
+  onChange,
+}: LabelledInputType) {
+  return (
+    <div>
+      <label className="block mb-2 text-sm text-black font-semibold pt-4">
+        {label}
+      </label>
+      <input
+        type={type || "text"}
+        id="first_name"
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder={placeholder}
+        onChange={onChange}
+        required
+      />
+    </div>
+  );
+}
