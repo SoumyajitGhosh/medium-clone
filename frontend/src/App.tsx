f18ee4f3f3f4cda40c15ccdc882b6d189d8fe33a
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-09-29 14:59:39 +0530

    FIX : Favicon, app name and default routing issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { Blog } from "./pages/Blog";
@@ -7,17 +7,16 @@ import { Publish } from "./pages/Publish";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blogs" element={<Blogs />} />
-          <Route path="/publish" element={<Publish />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/publish" element={<Publish />} />
+        <Route path="*" element={<Navigate to="/signup" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
