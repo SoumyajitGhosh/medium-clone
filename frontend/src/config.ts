8858489ad8d17db466554129a4899944754df128
SoumyajitGhosh <soumyajitghosh.official@gmail.com>
2024-10-09 12:27:17 +0530

    Fix:Render image

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = "https://backend.soumyajitghosh-official.workers.dev"
-export const BACKEND_URL = "http://localhost:8787"
+export const BACKEND_URL = "https://backend.soumyajitghosh-official.workers.dev"
